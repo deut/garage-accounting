a7334664d4f1f7f0e6856826dae6010f1d6caac4
Vitaliy Trofymiuk <vitaliy.trofymiuk@icloud.com>
2023-12-10 18:41:39 +0200

    Accounts list UI improvements

--- internal/app/ui/account_list.go
+++ internal/app/ui/account_list.go
@@ -2,14 +2,17 @@ package ui
 
 import (
 	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
 	"github.com/deut/garage-accounting/internal/services"
 )
 
 type AccountsList struct {
-	Window          fyne.Window
-	accountsService *services.Account
+	Window              fyne.Window
+	accountsService     *services.Account
+	contantTableHeaders []tableHeader
 }
 
 type tableHeader struct {
@@ -20,7 +23,20 @@ type tableHeader struct {
 }
 
 func NewAccountsList(w fyne.Window) AccountsList {
-	return AccountsList{Window: w, accountsService: services.New()}
+	return AccountsList{
+		Window:          w,
+		accountsService: services.New(),
+		contantTableHeaders: []tableHeader{
+			{placeholder: "", text: "ID", isSearchable: false, searchKey: "ID"},
+			{placeholder: "GarageNumber", text: "", isSearchable: true, searchKey: "GarageNumber"},
+			{placeholder: "FullName", text: "", isSearchable: true, searchKey: "FullName"},
+			{placeholder: "PhoneNumber", text: "", isSearchable: true, searchKey: "PhoneNumber"},
+			{placeholder: "", text: "Address", isSearchable: false},
+			{placeholder: "", text: "Ballans", isSearchable: false},
+			{placeholder: "          ", text: "", isSearchable: false},
+			{placeholder: "          ", text: "", isSearchable: false},
+		},
+	}
 }
 
 func (al *AccountsList) Build() fyne.CanvasObject {
@@ -30,42 +46,60 @@ func (al *AccountsList) Build() fyne.CanvasObject {
 		accsTableContent = [][]string{}
 	}
 
-	headers := []tableHeader{
-		{placeholder: "", text: "ID", isSearchable: false, searchKey: "ID"},
-		{placeholder: "GarageNumber", text: "", isSearchable: true, searchKey: "GarageNumber"},
-		{placeholder: "FullName", text: "", isSearchable: true, searchKey: "FullName"},
-		{placeholder: "PhoneNumber", text: "", isSearchable: true, searchKey: "PhoneNumber"},
-		{placeholder: "", text: "Address", isSearchable: false},
-		{placeholder: "", text: "Ballans", isSearchable: false},
-		{placeholder: "", text: "", isSearchable: false},
-	}
-
-	table := &widget.Table{}
-	table = widget.NewTable(
+	table := widget.NewTable(
 		func() (int, int) {
 			if len(accsTableContent) > 0 {
-				return len(accsTableContent), len(accsTableContent[0])
+				return len(accsTableContent), len(accsTableContent[0]) + 2
 			} else {
-				return 0, len(headers)
+				return 0, len(al.contantTableHeaders)
 			}
 		},
-		func() fyne.CanvasObject { return widget.NewLabel("") },
+		func() fyne.CanvasObject {
+			return container.NewStack(
+				widget.NewLabel(""),
+				widget.NewButton("", func() {}),
+			)
+		},
 		func(i widget.TableCellID, o fyne.CanvasObject) {
 			if len(accsTableContent) > 0 {
-				o.(*widget.Label).SetText(accsTableContent[i.Row][i.Col])
+
+				// TODO: handle OK
+				c := o.(*fyne.Container)
+				l := c.Objects[0].(*widget.Label)
+				b := c.Objects[1].(*widget.Button)
+
+				if len(accsTableContent[0]) > i.Col {
+					b.Hide()
+					l.Show()
+					l.SetText(accsTableContent[i.Row][i.Col])
+				} else {
+					l.Hide()
+					b.Show()
+					switch i.Col - len(accsTableContent[0]) {
+					case 0:
+						b.SetText("edit")
+					case 1:
+						b.SetText("receipt")
+					}
+				}
 			}
 		})
 
-	table.CreateHeader = func() fyne.CanvasObject { return widget.NewEntry() }
-	table.UpdateHeader = func(id widget.TableCellID, template fyne.CanvasObject) {
-		entry := template.(*widget.Entry)
-		if headers[id.Col].isSearchable {
-			entry.SetPlaceHolder("🔍  " + headers[id.Col].placeholder)
-			entry.OnChanged = func(s string) {
+	table.CreateHeader = func() fyne.CanvasObject {
+		return container.New(layout.NewStackLayout(), widget.NewLabel(""), widget.NewEntry())
+	}
 
-				table.UpdateHeader
+	table.UpdateHeader = func(id widget.TableCellID, o fyne.CanvasObject) {
+		c := o.(*fyne.Container)
+		l := c.Objects[0].(*widget.Label)
+		e := c.Objects[1].(*widget.Entry)
 
-				header := headers[id.Col]
+		if al.contantTableHeaders[id.Col].isSearchable {
+			l.Hide()
+			e.Show()
+			e.SetPlaceHolder("🔍  " + al.contantTableHeaders[id.Col].placeholder)
+			e.OnChanged = func(s string) {
+				header := al.contantTableHeaders[id.Col]
 				r, err := al.accountsService.Search(header.searchKey, s)
 
 				if err != nil {
@@ -76,36 +110,27 @@ func (al *AccountsList) Build() fyne.CanvasObject {
 				}
 			}
 		} else {
-			entry.SetText(headers[id.Col].text)
-			entry.Disable()
+			l.Show()
+			e.Hide()
+			l.SetText(al.contantTableHeaders[id.Col].text)
 		}
 	}
 
 	table.ShowHeaderRow = true
 
-	for i, h := range headers {
+	for i, h := range al.contantTableHeaders {
 		var width float32
 		if h.isSearchable {
 			width = float32(18 * len(h.placeholder))
 		} else {
 			width = float32(18 * len(h.text))
 		}
-		table.SetColumnWidth(i, width)
-	}
 
-	table.OnSelected = func(id widget.TableCellID) {
-		table.UnselectAll()
-		dialog.NewForm(
-			"receipt",
-			"checkout",
-			"cancel",
-			[]*widget.FormItem{
-				widget.NewFormItem("summ", widget.NewEntry()),
-				widget.NewFormItem("type", widget.NewSelectEntry([]string{"rent", "electicity"})),
-			},
-			func(b bool) {},
-			al.Window,
-		).Show()
+		if width == 0 {
+			width = float32(100)
+		}
+
+		table.SetColumnWidth(i, width)
 	}
 
 	return table
